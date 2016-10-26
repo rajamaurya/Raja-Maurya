81b38884d8ff32b0b941b6a8ede57078562261cb
rajamaurya <krishna.raja78@gmail.com>
2016-10-26 19:40:40 +0530

    Add files via upload

--- ConnectionController.cs
+++ ConnectionController.cs
@@ -23,18 +23,18 @@ namespace WebApplication3.Controllers
                      
                   connection.Open();
 
-                  string save = "insert into SignUp values('raja', 'maurya', 'male',' rkm', 'krgfcfg', Convert(DateTime,'19820626',112), 464644,64644)";
-                  //string save = "insert into SignUp values(@[First Name], @[Last Name], @sex,  @[User Name], @Email, @[Date Of Birth], @[Mobile No], @Passowrd)";
+                  //string save = "insert into SignUp values('raja', 'maurya', 'male',' rkm', 'krgfcfg', Convert(DateTime,'19820626',112), 464644,64644)";
+                  string save = "insert into SignUp values(@FirstName, @LastName, @Sex,  @UserName, @Email, @DateOfBirth, @MobileNo, @Password)";
                       SqlCommand cmnd = new SqlCommand(save, connection);
-                    //  cmnd.Parameters.AddWithValue("@[First Name]", signup.FirstName);
-                    //  cmnd.Parameters.AddWithValue("@[Last Name]", signup.LastName);
-                    //  cmnd.Parameters.AddWithValue("@sex", signup.sexid);
-                    //  cmnd.Parameters.AddWithValue("@[User Name]", signup.UserName);
-                      
-                    //  cmnd.Parameters.AddWithValue("@Email", signup.Email);
-                    //  cmnd.Parameters.AddWithValue("@[Date Of Birth]", signup.date);
-                    //  cmnd.Parameters.AddWithValue("@[Mobile No]", signup.MobileNo);
-                    //cmnd.Parameters.AddWithValue("@Passowrd", signup.Key);
+                      cmnd.Parameters.AddWithValue("@FirstName", signup.FirstName);
+                      cmnd.Parameters.AddWithValue("@LastName", signup.LastName);
+                      cmnd.Parameters.AddWithValue("@Sex", signup.sexid);
+                      cmnd.Parameters.AddWithValue("@UserName", signup.UserName);
+
+                      cmnd.Parameters.AddWithValue("@Email", signup.Email);
+                      cmnd.Parameters.AddWithValue("@DateOfBirth",System.DateTime.Now);
+                      cmnd.Parameters.AddWithValue("@MobileNo", signup.MobileNo);
+                      cmnd.Parameters.AddWithValue("@Password", signup.Key);
                       cmnd.ExecuteNonQuery();
                   
                     
@@ -51,25 +51,29 @@ namespace WebApplication3.Controllers
                 string get = "select * from dbo.SignUp";
                 SqlCommand cmd = new SqlCommand(get, connection);
                 SqlDataReader dr = cmd.ExecuteReader();
-                while (dr.Read())
-                {
-                    SignUp e = new SignUp();
-                    e.FirstName = Convert.ToString(dr[0]);
-                    e.LastName = Convert.ToString(dr[1]);
-                 //   e.sexid = Convert.ToInt32(dr[2]);
-                    e.UserName = Convert.ToString(dr[3]);
-                    e.Key = Convert.ToString(dr[4]);
-                    e.Email = Convert.ToString(dr[5]);
-                    e.date =Convert.ToInt32(dr[6]);
-                    e.MobileNo = Convert.ToInt32(dr[7]);
-                    
-                    signList.Add(e);
-                }
-                
-            
-            
+                 while (dr.Read())
+                  {
 
-            return signList;
+                      SignUp e = new SignUp();
+
+
+                      e.FirstName = Convert.ToString(dr[0]);
+                           e.LastName = Convert.ToString(dr[1]);
+                           e.FirstName = Convert.ToString(dr[2]);
+                           e.Id = Convert.ToInt16(dr[6]);
+                          //e.sexid = Convert.ToInt32(dr[2]);
+                           e.UserName = Convert.ToString(dr[4]);
+                           e.Key = Convert.ToString(dr[5]);
+                           //e.Email = dr.GetString(5);
+                           //e.date = Convert.ToDateTime(dr[6]);
+                           //e.MobileNo = dr.GetInt64(7); ;
+                          
+                      signList.Add(e);
+                  } 
+        
+           
+
+                return signList;
 
         }
 
@@ -78,19 +82,19 @@ namespace WebApplication3.Controllers
             connection.Open();
             string query = "delete from SignUp where Id=@Id";
             SqlCommand cmd = new SqlCommand(query, connection);
-            cmd.Parameters.AddWithValue("@id", data);
+            cmd.Parameters.AddWithValue("@Id", data);
             cmd.ExecuteNonQuery();
             connection.Close();
         }
         public void update(SignUp sign)
         {
             connection.Open();
-            string save = "update SignUp set FirstName=@[First Name], Password=@Pasword, MobileNo=@[Mobile No] where Id=@Id";
+            string save = "update SignUp set FirstName=@FirstName,MobileNo=@MobileNo where Id=@Id";
             SqlCommand cmd = new SqlCommand(save, connection);
-            cmd.Parameters.AddWithValue("@Id", sign.Id);
-            cmd.Parameters.AddWithValue("@[First Name]", sign.FirstName);
-            cmd.Parameters.AddWithValue("@Key", sign.Key);
-            cmd.Parameters.AddWithValue("@[Mobile No]", sign.MobileNo);
+           cmd.Parameters.AddWithValue("@Id", sign.Id);
+            cmd.Parameters.AddWithValue("@FirstName", sign.FirstName);
+         //   cmd.Parameters.AddWithValue("@Password", sign.Key);
+            cmd.Parameters.AddWithValue("@MobileNo", sign.MobileNo);
             cmd.ExecuteNonQuery();
             connection.Close();
         }
